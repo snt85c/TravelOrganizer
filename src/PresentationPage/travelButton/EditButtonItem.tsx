d6891afd73ffd400d84fb1e137310deb15302172
snt85c <santi.cacciola@hotmail.it>
2022-08-16 16:05:56 +0100

    minor style change

--- src/PresentationPage/travelButton/EditButtonItem.tsx
+++ src/PresentationPage/travelButton/EditButtonItem.tsx
@@ -15,11 +15,11 @@ export default function EditButtonItem(props: {
       {props.isAuthor() && (
         <div
           //edit, rename, delete buttons, if the user is also the author
-          className="flex justify-start items"
+          className="flex justify-center items w-1/3"
         >
           {!props.editingPackage.isEditing && (
             <div
-              className="text-[0.7rem] font-[homeworld-norm] cursor-pointer text-amber-500 duration-300 select-none "
+              className="text-[0.8rem] font-[homeworld-norm] cursor-pointer text-amber-500 duration-300 select-none "
               onClick={(e) => props.handleEdit(e)}
             >
               MODIFY
@@ -27,10 +27,10 @@ export default function EditButtonItem(props: {
           )}
           {props.editingPackage.isEditing && (
             <div ref={ref}>
-              <div className="flex flex-col justify-evenly items-center">
-                <div className="flex flex-row p-1 m-1">
+              <div className="flex flex-col justify-evenly items-center ">
+                <div className="flex flex-col p-1 m-1 gap-1">
                   <div
-                    className="mx-2 text-[0.7rem] p-2 font-[homeworld-norm] cursor-pointer text-gray-800 hover:text-amber-500 duration-300 select-none"
+                    className="mx-2 text-[0.7rem]  font-[homeworld-norm] cursor-pointer text-gray-800 hover:text-amber-500 duration-300 select-none"
                     onClick={() => {
                         props.editingPackage.setIsDeleting(!props.editingPackage.isDeleting);
                         props.editingPackage.setIsRenaming(false);
@@ -39,7 +39,7 @@ export default function EditButtonItem(props: {
                     DELETE
                   </div>
                   <div
-                    className="mx-2  text-[0.7rem] p-2 font-[homeworld-norm]  cursor-pointer text-gray-800 hover:text-amber-500 duration-300 select-none"
+                    className="mx-2  text-[0.7rem]  font-[homeworld-norm]  cursor-pointer text-gray-800 hover:text-amber-500 duration-300 select-none"
                     onClick={() => {
                         props.editingPackage.setIsRenaming(!props.editingPackage.isRenaming);
                         props.editingPackage.setIsDeleting(false);
@@ -50,6 +50,7 @@ export default function EditButtonItem(props: {
                 </div>
                 {props.editingPackage.isRenaming && (
                   <div
+                  className="flex flex-row"
                     onKeyDown={(e) => {
                       if (e.key === "Enter" && props.editingPackage.isRenaming) props.handleRename();
                     }}
@@ -64,11 +65,11 @@ export default function EditButtonItem(props: {
                 )}
                 {props.editingPackage.isDeleting && (
                   <div
-                    className=" flex flex-col justify-center items-center mx-2  text-sm cursor-pointer text-gray-800 hover:text-red-600 duration-300 hover:font-bold select-none"
+                    className=" flex flex-col justify-center items-center mx-2  text-sm cursor-pointer text-gray-800 hover:text-red-600 duration-300 select-none"
                     onClick={()=>props.handleDelete()}
                   >
-                    <div>press to delete</div>
-                    <div className="text-[0.5rem] -mt-2">
+                    <div className="text-center font-bold">press to delete</div>
+                    <div className="text-center leading-none text-[0.5rem]">
                       this will cancel your data permanently
                     </div>
                   </div>
